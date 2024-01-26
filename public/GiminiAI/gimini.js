355b256a0c4affbccaf211c0c6fdf7b947442759
jecko78 <jecko78@gmail.com>
2024-01-26 15:56:18 +0700

    web gimini GenAI

--- public/GiminiAI/gimini.js
+++ public/GiminiAI/gimini.js
@@ -1,19 +1,3 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const sapi = "AIzaSyBgTL7b7pqwm_e1ZhskR2_irx2_u8gj5yU"
 
-require("dotenv").config()
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.GIMINI_API_KEY);
-
-async function run() {
-  // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-
-  const prompt = "give me 8 reasons for learning in Australia."
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
-
-run();
+export {sapi};
