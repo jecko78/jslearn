b3011a2a7ce34a9f121c52a8a56cbcb4933a19cc
jecko78 <jecko78@gmail.com>
2024-02-22 14:22:03 +0700

    new sound.js file

--- public/sound02.js
+++ public/sound02.js
@@ -1,26 +1,53 @@
 const img01 = document.getElementById('img01');
 const img02 = document.getElementById('img02');
 const img03 = document.getElementById('img03');
-const img04 = document.getElementById('img04');
-const img05 = document.getElementById('img05');
-const img06 = document.getElementById('img06');
 
-let audio1 = new Audio();
-let audio2 = new Audio();
-let audio3 = new Audio();
-// console.log(audio1);
-audio1.src='LaoAphabet/1.mp3';
+
+function playalphabetsound(filename){
+  let audio2 = new Audio();
+	audio2.src = filename
+	audio2.play();
+};
+
 img01.addEventListener('click', function(){
-  audio1.play();
-  audio1.addEventListener('playing',function(){
-    console.log('Audio 1 started play!');
-  })  
-  audio1.addEventListener('ended',function(){
-    console.log('Audio 1 ended!');    
-  })
-  
+  let fname
+  fname="LaoAphabet/1.mp3";
+  playalphabetsound(fname);
 });
 
+img02.addEventListener('click', function(){
+  let fname
+  fname="LaoAphabet/2.mp3";
+  playalphabetsound(fname);
+});
+
+img03.addEventListener('click', function(){
+  let fname
+  fname="LaoAphabet/3.mp3";
+  playalphabetsound(fname);
+})
+
+img04.addEventListener('click', function(){
+  let fname
+  fname="LaoAphabet/4.mp3";
+  playalphabetsound(fname);
+});
+
+img05.addEventListener('click', function(){
+  let fname
+  fname="LaoAphabet/5.mp3";
+  playalphabetsound(fname);
+});
+
+img06.addEventListener('click', function(){
+  let fname
+  fname="LaoAphabet/6.mp3";
+  playalphabetsound(fname);
+});
+
+
+;
+/*
 audio2.src='LaoAphabet/2.mp3';
 img02.addEventListener('click', function(){
   audio2.play();
@@ -44,3 +71,4 @@ img03.addEventListener('click', function(){
   })
   
 });
+*/
